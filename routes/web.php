601548add10011cc33805e01c64437290eb0aad1
FunSkrip <flyiq440464@gmail.com>
2024-03-09 17:42:26 +0300

    Task 9.2

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::get('/', function () {
 Route::get('/user/{id?}', function ($id = null){
     return 'Пользователь '. $id; 
 });
+
+Route::get('/{year}/{month}/{day}/', function ($year, $month, $day) {
+    $year = (int)$year;
+    $month = (int)$month;
+    $day = (int)$day;
+    $date = "$year-$month-$day";
+
+    $weekDay = date('N', strtotime($date))-1;
+    $weekDaysNames = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+    return "День недели: $weekDaysNames[$weekDay]";
+})->where(['year' => '\d{4}', 'month' => '(0?[1-9]|1[0-2])', 'day' => '(0?[1-9]|[1-2][0-9]|3[0-1])']);
